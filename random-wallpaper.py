0c5a6b9681e7a6df931c6dfb3f6fc49c878026fd
Amir Sasani <sasani.amir.1998@gmail.com>
2020-05-29 02:05:34 +0430

    added gnome support

--- random-wallpaper.py
+++ random-wallpaper.py
@@ -1,54 +1,57 @@
 # usr/bin/python3
 
-# wget -O /home/amir/Pictures/Wallpapers/wallpaper.jpg https://unsplash.it/2560/1440/?random
-# gsettings set org.gnome.desktop.background picture-uri file:///home/amir/Pictures/Wallpapers/wallpaper.jpg
-
 import shutil
 import requests
 import os
 import tkinter
 import urllib.request
-import subprocess
+import shlex, subprocess
 import sys
 import platform
 
 
-base_url = "https://picsum.photos/"
+def download_wallpaper():
+    base_url = "https://picsum.photos/"
 
-root = tkinter.Tk()
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
+    root = tkinter.Tk()
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
 
-random_image_url = f"{base_url}{width}/{height}/?random"
+    random_image_url = f"{base_url}{width}/{height}/?random"
 
-home_directory = os.getenv("HOME")
+    home_directory = os.getenv("HOME")
 
-response = urllib.request.urlopen(random_image_url)
-print(response.getcode())
-if response.getcode() == 200:
-    image = urllib.request.urlretrieve(
-        random_image_url, f"{home_directory}/local-filename.jpg")
+    response = urllib.request.urlopen(random_image_url)
+    print(response.getcode())
+    if response.getcode() == 200:
+        urllib.request.urlretrieve(
+            random_image_url, f"{home_directory}/local-filename.jpg")
+        return f"{home_directory}/local-filename.jpg"
 
 
-def set_wallpaper(file_loc):
-    pass
+def set_wallpaper(file_location):
+
+    desktop_environment = detection()
+
+    print(desktop_environment)
+
+    if desktop_environment == "gnome":
+        command = ["gsettings", "set","org.gnome.desktop.background","picture-uri",f"file://{file_location}"]
+        subprocess.Popen(command).communicate()
 
 
 def detection():
 
-    if platform.system == 'Linux':
+    if platform.system() == 'Linux':
         desktops = {'gnome':'/usr/bin/gnome-session', 'mate':'/usr/bin/mate-session', 'lxde':'/usr/bin/lxsession', 'jwm':'/usr/bin/icewm-session'}
-        current_desktop = None
         for key, value in desktops.items():
             if os.path.exists(value):
-                current_desktop = key
-
-        if(current_desktop == 'gnome'):
-            pass
+                return key
     else:
-        print("Not Linux. Shame on you.")
+        return platform.system()
 
 
 
 if __name__ == "__main__":
-    print(platform.system())
+    image_uri = download_wallpaper()
+    set_wallpaper(image_uri)
