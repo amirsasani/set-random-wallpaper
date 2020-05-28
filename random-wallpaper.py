7d0ab7dc07cca0518294c3b4aa5a58ab5310d042
Amir Sasani <sasani.amir.1998@gmail.com>
2020-05-29 02:20:09 +0430

    Update random-wallpaper.py

--- random-wallpaper.py
+++ random-wallpaper.py
@@ -1,12 +1,9 @@
 # usr/bin/python3
 
-import shutil
-import requests
 import os
 import tkinter
 import urllib.request
-import shlex, subprocess
-import sys
+import subprocess
 import platform
 
 
@@ -22,34 +19,34 @@ def download_wallpaper():
     home_directory = os.getenv("HOME")
 
     response = urllib.request.urlopen(random_image_url)
-    print(response.getcode())
     if response.getcode() == 200:
         urllib.request.urlretrieve(
-            random_image_url, f"{home_directory}/local-filename.jpg")
-        return f"{home_directory}/local-filename.jpg"
+            random_image_url, f"{home_directory}/Pictures/Wallpapers/wallpaper.jpg")
+        return f"{home_directory}/Pictures/Wallpapers/wallpaper.jpg"
 
 
 def set_wallpaper(file_location):
-
     desktop_environment = detection()
 
-    print(desktop_environment)
-
     if desktop_environment == "gnome":
-        command = ["gsettings", "set","org.gnome.desktop.background","picture-uri",f"file://{file_location}"]
+        command = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f"file://{file_location}"]
         subprocess.Popen(command).communicate()
 
 
 def detection():
-
     if platform.system() == 'Linux':
-        desktops = {'gnome':'/usr/bin/gnome-session', 'mate':'/usr/bin/mate-session', 'lxde':'/usr/bin/lxsession', 'jwm':'/usr/bin/icewm-session'}
+        desktops = {
+            'gnome': '/usr/bin/gnome-session',
+            'mate': '/usr/bin/mate-session',
+            'lxde': '/usr/bin/lxsession',
+            'jwm': '/usr/bin/icewm-session'
+        }
+
         for key, value in desktops.items():
             if os.path.exists(value):
                 return key
     else:
         return platform.system()
-
 
 
 if __name__ == "__main__":
